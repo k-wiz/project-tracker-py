af1149b99cb01f23736a20aa76dd823b996361fa
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 16:02:25 -0700

    completed assign grade function

--- hackbright.py
+++ hackbright.py
@@ -44,7 +44,7 @@ def make_new_student(first_name, last_name, github):
     print "Successfully added student: %s %s" % (first_name, last_name)
 
 
-def get_project_by_title(title):
+def get_project_by_title(monkey_butt):
     """Given a project title, print information about the project."""
     QUERY = """
         SELECT description
@@ -52,7 +52,7 @@ def get_project_by_title(title):
         WHERE title = :cheesecake
         """
 
-    db_cursor = db.session.execute(QUERY, {'cheesecake': title})
+    db_cursor = db.session.execute(QUERY, {'cheesecake': monkey_butt})
     row = db_cursor.fetchone()
     print row[0]
 
@@ -64,7 +64,17 @@ def get_grade_by_github_title(github, title):
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+        INSERT INTO grades (student_github, project_title, grade) 
+        VALUES (:student_github_key, :project_title_key, :grade_key);
+        """
+
+    db_cursor = db.session.execute(QUERY, {'student_github_key': github, 'project_title_key': title,
+       'grade_key': grade})
+
+    print "Your grade is %s" %(grade)
+
+
 
 
 def handle_input():
@@ -92,6 +102,10 @@ def handle_input():
         elif command == "project_description":
             title = args[0]
             get_project_by_title(title)
+
+        elif command == "grade_student":
+            github, title, grade = args
+            assign_grade(github, title, grade)
 
         else:
             if command != "quit":
